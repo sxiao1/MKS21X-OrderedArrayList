97b105b802bd22b1e1dc19b3892bc75ade84cda2
sxiao1 <sxiao1@stuy.edu>
2018-11-02 11:00:25 -0400

    just stating

---
+++ OrderedArrayList.java
@@ -0,0 +1,11 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+	public OrderedArrayList(){
+		super();
+	}
+	public OrderedArrayList(int startingCapacity){
+		super(startingCapacity);
+	}
+	public T compareTo(T element, T element2){
+		return null;
+	}
+}

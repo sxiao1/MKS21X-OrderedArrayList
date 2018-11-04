48dca8b2bf8f9a92095eb984027a26d002d39fb0
sxiao1 <sxiao1@stuy.edu>
2018-11-03 22:42:19 -0400

    worked on adds and set

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,7 +5,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 	public OrderedArrayList(int startingCapacity){
 		super(startingCapacity);
 	}
-	public T compareTo(T element, T element2){
-		return null;
+	public int index(T value){
+		for(int i = 0; i < size(); i++){
+			if (value.compareTo(this.get(i)) <= 0){
+				return i;
+			}
+		}
+		return size();
+	}
+	public boolean add(T element){
+		int num = index(element);
+		super.add(num, element);
+		return this.get(num) == element;
+	}
+	public void add(int index, T element){
+		int num = index(element);
+		super.add(num, element);
+	}
+	public T set(int index, T element){
+		T element1 = super.get(index);
+		super.remove(index);
+		add(element);
+		return element1;
 	}
 }
